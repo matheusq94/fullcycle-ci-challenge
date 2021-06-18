1762bed6d14d1a22daffaf1bbd5b9132b4571f1f
Matheus Quintino de Paiva <matheus.q@live.com>
2021-06-18 17:48:17 -0300

    test: endpoint test

--- src/app.js
+++ src/app.js
@@ -4,11 +4,10 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', () => {
+app.get('/', (req, res) => {
     return res.status(200).json({
         message: 'Hello World!'
     })
 })
 
 module.exports = app
-// docker run --rm -it -v $(pwd)/:/usr/src/app -p 3000:3000 node:15 bash
